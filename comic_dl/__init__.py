73bfb7bfe229072e85dc07e926422b983e715beb
Xonshiz <kanojia24.10@gmail.com>
2017-12-28 08:21:20 +0530

    Fix for P3

--- comic_dl/__init__.py
+++ comic_dl/__init__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sites
+import manga_eden
 import comic_dl
 import globalFunctions
 from comic_dl import ComicDL
--- comic_dl/__main__.py
+++ comic_dl/__main__.py
@@ -4,7 +4,6 @@
 from comic_dl import ComicDL
 import sites
 import manga_eden
-# import comic_dl
 import sys
 
 if __name__ == "__main__":
