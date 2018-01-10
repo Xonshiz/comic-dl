acaee7e0e8f155b9cae9f10eab71f44ad1d8b9d7
Xonshiz <kanojia24.10@gmail.com>
2018-01-10 14:17:12 +0530

    minor fixes

--- comic_dl/exeMaker.bat
+++ comic_dl/exeMaker.bat
@@ -1,1 +1,3 @@
 pyinstaller --onefile --hidden-import=queue --icon="Logo.ico" "__main__.py"
+cd dist
+upx.exe "__main__.exe" --best
--- setup.py
+++ setup.py
@@ -5,21 +5,21 @@ history = open('Changelog.md').read()
 
 setup(
   name = 'comic_dl',
-  packages = ['comic_dl','comic_dl.sites','comic_dl.downloader'], # this must be the same as the name above
+  packages = ['comic_dl','comic_dl.sites', 'comic_dl.manga_eden'], # this must be the same as the name above
   install_requires=["selenium",
                       "requests",
                       "more_itertools",
                       "cfscrape",
                       "bs4"
     ],
-  version = '2017.01.22',
+  version = '2018.01.10',
   description = 'Comic-dl is a command line tool to download Comics and Manga from various Manga and Comic sites easily.',
   long_description=readme + '\n\n' + history,
   author = 'Xonshiz',
   author_email = 'xonshiz@psychoticelites.com',
   url='https://github.com/Xonshiz/comic-dl',
-  download_url = 'https://codeload.github.com/Xonshiz/comic-dl/legacy.tar.gz/v2016.11.26(1)',
-  keywords = ['comic-dl', 'cli', 'comic downloader','manga downloader','mangafox','batoto','kissmanga','comic naver'],
+  download_url = 'https://codeload.github.com/Xonshiz/comic-dl/tar.gz/2018.1.10',
+  keywords = ['comic-dl', 'cli', 'comic downloader','manga downloader','mangafox','batoto','kissmanga','comic naver','readcomiconline'],
   classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
